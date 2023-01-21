e1a9e1f15251279dbd3292358eedc5820a4c3c77
Victor Garcia Mestre <vgmestre@gmail.com>
2023-01-22 00:55:17 +0100

    feat: linter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-const res = await fetch('https://steamcommunity.com/id/vgmestre/');
-const text = await res.text();
+import cheerio from 'cheerio';
 
-console.log(text);
+const BASE_URL = 'https://steamcommunity.com/';
+
+function getUserUrl(userId: string) {
+  return `${BASE_URL}/id/${userId}`;
+}
+
+async function scrape(url: string) {
+  const res = await fetch(url);
+  const html = await res.text();
+  return cheerio.load(html);
+}
+
+/* const $ = cheerio.load(html);
+console.log('started scraping');
+console.log(html);
+$('.recent_game_content .game_info .game_name a').each((index, el) =>
+  console.log($(el).text())
+); */
 
 export {};
